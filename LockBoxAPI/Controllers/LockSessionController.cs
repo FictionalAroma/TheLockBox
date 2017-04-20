9fbb5291fce04d4d6d19caccd85d13ea315a2844
Fictional <fictionalaroma@gmail.com>
2017-04-20 15:43:04 +0100

    AddingModel and sorting routing (needed ApiController)

---
+++ LockBoxAPI/Controllers/LockSessionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace LockBoxAPI.Controllers
+{
+    public class LockSessionController : ApiController
+    {
+        // GET: LockSession
+        public ActionResult GetEndDateTime()
+        {
+            return new JsonResult {Data = DateTime.Now};
+        }
+    }
+}
